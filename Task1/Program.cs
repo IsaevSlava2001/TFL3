8c6ad276bd12176cb85f29696e2a82e8142e7d2f
Trebon_Deon <IsaevSlava.2001@yandex.ru>
2022-09-17 14:44:46 +0300

    task1 and task2

---
+++ Task1/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication18
+{
+    class Program
+    {
+        public static bool IsNumeric(char n)
+        {
+            if(n=='0'||n=='1'||n=='2'||n=='3'||n=='4'||n=='5'||n=='6'||n=='7'||n=='8'||n=='9')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static int CheckPriority(char n)
+        {
+            if (n == '*' || n == '/') return 3;
+            if (n == '+' || n == '-') return 2;
+            if (n == '(') return 1;
+            else return 0;
+        }
+        static void Main(string[] args)
+        {
+            string str;
+            string OutputStr="";
+            Console.WriteLine("Введите строку со всеми символами");
+            str = Console.ReadLine();
+            Stack<string> num = new Stack<string>();
+            for(int i=0;i<str.Length;i++)
+            {
+                if(IsNumeric(str[i]))
+                {
+                    OutputStr += str[i];
+                }
+                else
+                {
+                    int pri = CheckPriority(str[i]);
+                    int priStack=0;
+                    string stack;
+                    try
+                    {
+                        stack = num.Peek();
+                    }
+                    catch(Exception e)
+                    {
+                        stack = "";
+                    }
+                    if(stack!="")
+                    {
+                        priStack = CheckPriority(Convert.ToChar(stack));
+                    }
+                    //3В
+                    if (str[i] == '(')
+                    {
+                        num.Push(Convert.ToString(str[i]));
+                    }
+                    //---------------------------
+                    //3Г
+                    else if (str[i] == ')')
+                    {
+                        while (num.Peek() != "(")
+                        {
+                            OutputStr += num.Pop();
+                        }
+                        num.Pop();
+                    }
+                    //-----------------
+                    //3А
+                    else if (stack==""||priStack<pri)
+                    {
+                        num.Push(Convert.ToString(str[i]));
+                    }
+                    //----------------------
+                    //3Б
+                    else if(priStack==pri||priStack>pri)
+                    {
+                        while(priStack == pri || priStack > pri)
+                        {
+                            string temp = num.Pop();
+                            OutputStr += temp;
+                            try
+                            {
+                                priStack = CheckPriority(Convert.ToChar(num.Peek()));
+                            }
+                            catch (Exception e)
+                            {
+                                break;
+                            }
+                        }
+                        num.Push(Convert.ToString(str[i]));
+                    }
+                    //-----------------------
+                }
+            }
+            bool IsStackFill = true;
+            while (IsStackFill)
+            {
+                try
+                {
+                    OutputStr += num.Pop();
+
+                }
+                catch (Exception e)
+                {
+                    IsStackFill = false;
+                }
+            }
+            Console.WriteLine(OutputStr);
+            File.WriteAllText("rez.txt", OutputStr);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        public static bool IsNumeric(char n)
+        {
+            if (n == '0' || n == '1' || n == '2' || n == '3' || n == '4' || n == '5' || n == '6' || n == '7' || n == '8' || n == '9')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1-Ввод строки вручную\n2-ввод строки из файла");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Stack<string> num = new Stack<string>();
+            string InputStr="";
+            switch(a)
+            {
+                case 1:
+                    Console.WriteLine("Введите строку в ОПЗ");
+                    InputStr = Console.ReadLine();
+                    break;
+                case 2:
+                    InputStr = File.ReadAllText("C:/Users/1655299/Documents/Visual Studio 2015/Projects/ConsoleApplication18/ConsoleApplication18/bin/Debug/rez.txt");
+                    break;
+                default:
+                    Console.WriteLine("Ошибка");
+                        Console.ReadLine();
+                        Environment.Exit(1);
+                    break;
+            }
+            for (int i=0;i<InputStr.Length;i++)
+            {
+                if(IsNumeric(InputStr[i]))
+                {
+                    num.Push(Convert.ToString(InputStr[i]));
+                }
+                else
+                {
+                    int k = Convert.ToInt32(num.Pop());
+                    int b = Convert.ToInt32(num.Pop());
+                    int rez = 0;
+                    switch(InputStr[i])
+                    {
+                        case '+':
+                            rez = b + k;
+                            break;
+                        case '-':
+                            rez = b - k;
+                            break;
+                        case '*':
+                            rez = b * k;
+                            break;
+                        case '/':
+                            rez = b / k;
+                            break;
+                    }
+                    num.Push(Convert.ToString(rez));
+                }
+            }
+            Console.WriteLine(num.Pop());
+            Console.ReadLine();
+
+        }
+    }
+}
